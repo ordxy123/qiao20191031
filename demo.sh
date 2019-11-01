e4e78109f390fa508772a18cf56082a42168d574
qiao <776890692@qq.com>
2019-11-01 15:16:44 +0800

    第四次提交

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#集群节点（一台机器安装多个节点，端口不同）
 
+#集群节点（一台机器安装多个节点，端口不同）
 #ip:7001
 #ip:7002
 #ip:7003
@@ -14,17 +14,33 @@
 #redis下载链接,版本为4.0.9
 redisUrl='http://download.redis.io/releases/redis-4.0.9.tar.gz'
 
+#判断系统类型 yum/apt
+#if [ $(cat /etc/redhat-release | grep CentOS)];then
+
+#判断系统版本
+
 #下载redis
-#需要判断 wget 是否安装；暂时不会写
-wget $redisUrl
+#判断 wget 是否安装
+if [ $(rpm -qa | grep wget) == "" ];then
+#根据系统类型和版本判断执行命令；暂时不会写
+	yum -y install wget	
+fi
+
+wget $redisUrl -O redis.tar.gz
 
 #解压
-#需要判断 tar 是否安装
-tar -zxvf redis-4.0.9.tar.gz
+#判断 tar 是否安装
+if [ $(rpm -qa | grep tar) == "" ];then
+#根据系统类型和版本判断执行命令
+	yum -y install tar	
+fi
+
+tar -zxvf redis.tar.gz
+mv $(ls | grep redis-) redis
 
 #编译安装
 #需要判断 c 环境是否搭建
-cd redis-4.0.9/
+cd redis/
 make && make install
 
 #
@@ -32,15 +48,15 @@ cd src/
 cp redis-trib.rb /usr/local/bin/
 
 #
-mkdir -p /opt/redis-4.0.9/redis_cluster
-cp ../redis.conf /opt/redis-4.0.9/redis_cluster/
+mkdir -p /opt/redis/redis_cluster
+cp ../redis.conf /opt/redis/redis_cluster/
 
 #复制redis.conf配置文件命名为redis-7001.conf，redis-7002.conf，redis-7003.conf，redis-7004.conf，redis-7005.conf，redis-7006.conf
-cd /opt/redis-4.0.9/redis_cluster/
+cd /opt/redis/redis_cluster/
 #设置需要配置redis的个数
 num=7
 #复制
-#配置文件拼接字符串
+#配置文件命名拼接字符串
 pre='redis.700'
 after='.conf'
 for(i = 0; i < $num; i++));
@@ -80,7 +96,7 @@ def funmain(){
 funmain();
 
 #启动redis
-cd /opt/redis-4.0.9.redis_cluster/
+cd /opt/redis/redis_cluster/
 for(i = 0; i < $num; i++));
 do
 redis-server $pre$i$after
@@ -96,7 +112,10 @@ gem install redis
 
 #报错则执行;
 #判断是否安装 curl
-#yum install curl
+if [ $(rpm -qa | grep curl) == "" ];then
+#根据系统类型和版本判断执行命令；暂时不会写
+	yum -y install curl	
+fi
 #安装 rvm；报错：curl: (6) Couldn't resolve host 'get.rvm.io'选择第二条执行
 curl -L get.rvm.io | bash -s stable 
 #curl -L https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable
@@ -116,7 +135,12 @@ rvm use 2.3.4
 gem install redis
 
 #创建redis集群
-redis-trib.rb  create  --replicas  1  $ip:7001 $ip:7002 $ip:7003 $ip:7004 $ip:7005 $ip:7006
+str=':700'
+portPre=$ip$str
+for(i = 0; i < $num; i++));
+do
+redis-trib.rb create --replicas 1 $portPre$i
+done
 
 #连接集群
 #redis-cli -h ip -c -p port
